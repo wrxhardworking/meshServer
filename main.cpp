e5acbc615c22d1edadbd4e8496a38f36eed6037f
wrx <2810852875@qq.com>
2024-07-17 00:20:19 +0800

    first

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <SMESH_SMESH.hxx>
+#include <SMESH_Gen.hxx>
+#include <SMESH_Mesh.hxx>
+
+int main() {
+//    InitLog();
+    for (int i = 0; i < 100; ++i) {
+    }
+    return 0;
+}
